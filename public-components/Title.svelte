b70425cb03f0c3b82c87ad82f2152bc3527e55f7
Daman56100 <samcraft792@gmail.com>
2020-07-09 21:44:20 +0100

    Added title component

---
+++ public-components/Title.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let text;
+</script>
+
+<style>
+    p {
+        color: #EB2A00;
+        margin-top: 50px;
+        font-size: 40px;
+        font-family: 'Viga';
+        text-align: center;
+    }
+</style>
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Viga" />
+<p>{text}</p>
