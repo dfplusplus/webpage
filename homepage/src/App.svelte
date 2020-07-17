f3fc826b5b16bbff59c47931fda8e253674ee915
Daman56100 <samcraft792@gmail.com>
2020-07-17 10:32:59 +0100

    Homepage now retrieves recommended release

--- homepage/src/App.svelte
+++ homepage/src/App.svelte
@@ -1,6 +1,14 @@
 <script>
   import Background from "../../public-components/Background.svelte";
   import Button from "../../public-components/Button.svelte";
+
+  async function getLatestRelease() {
+		let releasesSnapshot = await firebase.database().ref("releases").once('value');
+    let releases = releasesSnapshot.val();
+		let recommendedSnapshot = await firebase.database().ref("recommended").once('value');
+    let recommended = recommendedSnapshot.val();
+    return releases[recommended];
+  }
 </script>
 
 <style>
@@ -36,7 +44,11 @@
 <main>
   <img src="logo.png" alt="logo" class="centred"/>
   <p class="subtitle">1-up your DiamondFire experience</p>
-  <Button title="Download" href="https://dfplusplus.github.io/webpage/downloads/" important subtitle="BETA-5"/>
+  {#await getLatestRelease()}
+    <Button title="Download" href="" important subtitle="Retrieving Recommended Download..."/>
+  {:then latestRelease}
+    <Button title="Download" href={latestRelease["default"]} important subtitle="v{latestRelease.release.replace(/-/g,".")} for {latestRelease.mcRelease}"/>
+  {/await}
   <Button title="All downloads" href="https://dfplusplus.github.io/webpage/downloads/"/>
   <Button title="Features" href="https://dfplusplus.github.io/webpage/features/"/>
   <Button title="Credits" href="https://dfplusplus.github.io/webpage/credits/"/>
